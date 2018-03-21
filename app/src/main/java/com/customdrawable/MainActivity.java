e22910e63229883a743406a13caf67c223ae6d24
zhao <18337104423@163.com>
2018-03-21 16:46:16 +0800

    clipdrawable

--- app/src/main/java/com/customdrawable/MainActivity.java
+++ app/src/main/java/com/customdrawable/MainActivity.java
@@ -2,7 +2,9 @@ package com.customdrawable;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.ClipDrawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -17,7 +19,10 @@ public class MainActivity extends AppCompatActivity {
     private ImageView roundimage;
     private ImageView circleimage;
     private BitmapShaderView shaderview;
+    private ImageView clipview;
 
+    int level;
+    private Handler mHandler = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +36,23 @@ public class MainActivity extends AppCompatActivity {
         //圆形
         circleimage.setImageDrawable(new CircleDrawable(bitmap));
 
+        final ClipDrawable clipDrawable= (ClipDrawable) clipview.getDrawable();
+
+        level=clipDrawable.getLevel();
+
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                clipDrawable.setLevel(level);
+                level+=200;
+
+                if (level<=10000){
+                    mHandler.postDelayed(this,100);
+                }
+            }
+        },100);
+
+
     }
 
     private void initView() {
@@ -38,5 +60,6 @@ public class MainActivity extends AppCompatActivity {
         roundimage = (ImageView) findViewById(R.id.roundimage);
         circleimage = (ImageView) findViewById(R.id.circleimage);
         shaderview = (BitmapShaderView) findViewById(R.id.shaderview);
+        clipview = (ImageView) findViewById(R.id.clipview);
     }
 }
---
+++ app/src/main/java/com/customdrawable/view/ClipDrawableView.java
@@ -0,0 +1,28 @@
+package com.customdrawable.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Administrator on 2018/3/21.
+ */
+
+public class ClipDrawableView extends View {
+
+    public ClipDrawableView(Context context) {
+        super(context);
+    }
+
+    public ClipDrawableView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ClipDrawableView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public void init(){
+
+    }
+}
